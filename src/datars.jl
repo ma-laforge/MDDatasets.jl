b76f3532a1429da0baa3dca6c2d2fbd042ee694d
MA Laforge <ma.laforge@example.com>
2016-02-01 17:40:41 -0500

    Add functionality to DataRS
    Add constructor, Add promote_rule
    Add support for parameter(DataRS, ...)
    
    Also:
    Improve show function & add new constructor.
    Use ArgumentError instead of DomainError
    (DomainError does not allow message)

--- src/datars.jl
+++ src/datars.jl
@@ -26,6 +26,20 @@ elemallowed(::Type{DataRS}, ::Type{DataRS}) = true #Also allow recursive structu
 #Shorthand (because default (non-parameterized) constructor was overwritten):
 DataRS{T}(sweep::PSweep, elem::Vector{T}) = DataRS{T}(sweep, elem)
 
+#Generate empty DataRS structure:
+call{T}(::Type{DataRS{T}}, sweep::PSweep) = DataRS{T}(sweep, Array(T, length(sweep)))
+
+
+#==Type promotions
+===============================================================================#
+Base.promote_rule{T1<:DataRS, T2<:Number}(::Type{T1}, ::Type{T2}) = DataRS
+
+
+#==Accessor functions
+===============================================================================#
+Base.eltype{T}(d::DataRS{T}) = T
+Base.length(d::DataRS) = length(d.elem)
+
 
 #==Help with construction
 ===============================================================================#
@@ -42,24 +56,85 @@ Base.fill{T}(fn::Function, ::Type{DataRS{T}}, sweep::PSweep) =
 Base.fill(fn::Function, ::Type{DataRS}, sweep::PSweep) = fill(fn, DataRS{DataRS}, sweep)
 
 
+#==Data generation
+===============================================================================#
+function _ensuresweepunique(d::DataRS, sweepid::AbstractString)
+	if sweepid == d.sweep.id
+		msg = "Sweep occurs multiple times in DataRS: $sweepid"
+		throw(ArgumentError(msg))
+	end
+end
+
+#Define "parameter".
+#(Generates a DataRS object containing the value of a given swept parameter)
+#-------------------------------------------------------------------------------
+
+#Deal with non-leaf elements, once the sweep value is found:
+function _parameter{T}(d::DataRS{DataRS}, sweepid::AbstractString, sweepval::T)
+	_ensuresweepunique(d, sweepid)
+	elem = DataRS[_parameter(d.elem[i], sweepid, sweepval) for i in 1:length(d.sweep)]
+	return DataRS(d.sweep, elem)
+end
+
+#Deal with leaf elements, once the sweep value is found:
+function _parameter{T}(d::DataRS, sweepid::AbstractString, sweepval::T)
+	_ensuresweepunique(d, sweepid)
+	elem = T[sweepval for i in 1:length(d.sweep)]
+	return DataRS(d.sweep, elem)
+end
+
+#Main "parameter" algorithm (non-leaf elements):
+function parameter(d::DataRS{DataRS}, sweepid::AbstractString)
+	if sweepid == d.sweep.id #Sweep found
+		elem = DataRS[_parameter(d.elem[i], sweepid, d.sweep.v[i]) for i in 1:length(d.sweep)]
+	else
+		elem = DataRS[parameter(d.elem[i], sweepid) for i in 1:length(d.sweep)]
+	end
+	return DataRS(d.sweep, elem)
+end
+#Main "parameter" algorithm (leaf elements):
+function parameter(d::DataRS, sweepid::AbstractString)
+	T = eltype(d.sweep.v)
+	if sweepid == d.sweep.id #Sweep found
+		return DataRS(d.sweep, d.sweep.v)
+	else
+		msg = "Sweep not found in DataRS: $sweepid"
+		throw(ArgumentError(msg))
+	end
+end
+
+
 #==User-friendly show functions
 ===============================================================================#
 
-function showDataRS(io::IO, ds::DataRS, indent::AbstractString="")
-	for i in 1:length(ds.elem)
-		println(io, "$indent", ds.sweep.id, "=", ds.sweep.v[i], ": ", ds.elem[i])
+#Print leaf element:
+function printDataRSelem(io::IO, ds::DataRS, idx::Int, indent::AbstractString)
+	if isdefined(ds.elem, idx)
+		println(io, ds.elem[idx])
+	else
+		println(io, indent, "UNDEFINED")
 	end
 end
-function showDataRS{T<:DataRS}(io::IO, ds::DataRS{T}, indent::AbstractString="")
+#Print next level of recursive DataRS:
+function printDataRSelem{T<:DataRS}(io::IO, ds::DataRS{T}, idx::Int, indent::AbstractString)
+	println(io)
+	if isdefined(ds.elem, idx)
+		printDataRS(io, ds.elem[idx], indent)
+	else
+		println(io, indent, "UNDEFINED")
+	end
+end
+#Print DataRS structure:
+function printDataRS(io::IO, ds::DataRS, indent::AbstractString)
 	for i in 1:length(ds.elem)
-		println(io, "$indent", ds.sweep.id, "=", ds.sweep.v[i], ":")
-		showDataRS(io, ds.elem[i], "$indent  ")
+		print(io, "$indent", ds.sweep.id, "=", ds.sweep.v[i], ": ")
+		printDataRSelem(io, ds, i, "$indent  ")
 	end
 end
 
 function Base.show(io::IO, ds::DataRS)
 	print(io, "DataRS[\n")
-	showDataRS(io, ds, "  ")
+	printDataRS(io, ds, "  ")
 	print(io, "]\n")
 end
 
