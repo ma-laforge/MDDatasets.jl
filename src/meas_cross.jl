2eab8a765dd2ff7a67c23e3f3aad6499293c0ae3
MA Laforge <ma.laforge@example.com>
2016-02-05 14:05:15 -0500

    Strip out code moved to meas_cksignals.jl.
    (measperiod & measfreq)

--- src/meas_cross.jl
+++ src/meas_cross.jl
@@ -246,25 +246,4 @@ function ycross1{TX<:Number, TY<:Number, T<:DF1_Num}(
 end
 
 
-#==Crossing-based functions
-===============================================================================#
-
-#measperiod: Measure period between successive zero-crossings:
-#-------------------------------------------------------------------------------
-function measperiod(d::DataF1; nmax::Integer=0, tstart::Real=-Inf,
-	xing::CrossType=CrossType(), shiftx=true)
-
-	dx = xcross(d, nmax=nmax, tstart=tstart, allow=xing)
-	return deltax(dx, shiftx=shiftx)
-end
-
-#measfreq: Measure 1/period between successive zero-crossings:
-#-------------------------------------------------------------------------------
-function measfreq(d::DataF1; nmax::Integer=0, tstart::Real=-Inf,
-	xing::CrossType=CrossType(), shiftx=true)
-
-	T = measperiod(d, nmax=nmax, tstart=tstart, xing=xing, shiftx=shiftx)
-	return DataF1(T.x, 1./T.y)
-end
-
 #Last line
